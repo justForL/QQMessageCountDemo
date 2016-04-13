8d17574ae04bd021c286c46dabd350841f903b44
James_Leng <layer_james@163.com>
2016-04-13 23:12:26 +0800

    Initial Commit

---
+++ QQMessageCountDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQMessageCountDemo
+//
+//  Created by James on 16/4/13.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
